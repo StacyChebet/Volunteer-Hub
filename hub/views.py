c589a6efb33057d5eb1b6f8966c04b129685adf9
StacyChebet <staceychebet23@gmail.com>
2019-01-19 21:15:25 +0300

    Create view function for index page

---
+++ hub/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+from django.contrib.auth.decorators import login_required
+import datetime as dt
+from django.contrib.auth.models import User
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+    
